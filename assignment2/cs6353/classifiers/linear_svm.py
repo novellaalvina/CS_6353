61e979b3bebe476fa55a6b473bcc2ea7a19685f5
novellaalvina <112059794+novellaalvina@users.noreply.github.com>
2024-09-24 01:51:57 -0600

    implement svm loss naive method in linear_svm.py and run it in svm.py

--- assignment2/cs6353/classifiers/linear_svm.py
+++ assignment2/cs6353/classifiers/linear_svm.py
@@ -19,6 +19,15 @@ def svm_loss_naive(W, X, y, reg):
   - loss as single float
   - gradient with respect to weights W; an array of same shape as W
   """
+  #############################################################################
+  # TODO:                                                                     #
+  # Compute the gradient of the loss function and store it dW.                #
+  # Rather that first computing the loss and then computing the derivative,   #
+  # it may be simpler to compute the derivative at the same time that the     #
+  # loss is being computed. As a result you may need to modify some of the    #
+  # code above to compute the gradient.                                       #
+  #############################################################################
+
   dW = np.zeros(W.shape) # initialize the gradient as zero
 
   # compute the loss and the gradient
@@ -35,6 +44,18 @@ def svm_loss_naive(W, X, y, reg):
       if margin > 0:
         loss += margin
 
+        """L(W) = sum(L_i)/N + lambda * R(W)
+           sum(L_i)/N is the data loss part. lambda * R(W) is the regularization part.
+           L_i = sum [max(0, w_j * x_i - w_y_i * x_i + delta)]
+           R(W) = sum_of_k sum_of_l (W_k,l)^2   -> L2 regularization
+        """
+        # step 1
+        # update the loss gradient (data loss) in respect to the weight based on the classes
+        """dL_i/dW_y[i] = -(sum(indicator_1(loss)))x_i"""
+        """dL_i/dW_j = (sum(indicator_1(loss)))x_i"""
+        dW[:, y[i]] -= X[i] # with respect to the class
+        dW[:, j] += X[i]    # with respect to the incorrect class
+
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
@@ -42,15 +63,16 @@ def svm_loss_naive(W, X, y, reg):
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
 
-  #############################################################################
-  # TODO:                                                                     #
-  # Compute the gradient of the loss function and store it dW.                #
-  # Rather that first computing the loss and then computing the derivative,   #
-  # it may be simpler to compute the derivative at the same time that the     #
-  # loss is being computed. As a result you may need to modify some of the    #
-  # code above to compute the gradient.                                       #
-  #############################################################################
-  pass
+  # step 2
+  # update the loss gradient (data loss) in respect to the weight for the overall loss gradient
+  """dL/dW for the data loss part is the current (dL_i/dW)/N"""
+  dW /= len(X)
+  # update the loss gradient (regularization) in respect to the weight
+  """Let f is the regularization part of the loss function. So df/dW = 2 * lambda * W_k,l.  
+     Hence, the overall loss gradient in respect to weight is the sum of the loss gradient of the data loss and regularization part.
+     dL/dW = dL_i/dW + df/dW"""
+  dW += (2* reg * W)
+  
   #####################################################################
   #                       END OF YOUR CODE                            #
   #####################################################################
