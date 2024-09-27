implement both softmax loss naive and softmax loss vectorized, run in softmax.ipynb. but vectorized is still incorrect.

assignment2/cs6353/classifiers/softmax.py
@@ -29,7 +29,47 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  """L_i = -log(exp(s_y_i) / sum(exp(s_j)))
+         = -s_y_i + log(sum(e^(s_j)))
+      
+      s = w * x_i, j = 0, ..., C"""
+  
+  for i in range(len(X)):
+    scores = X[i].dot(W)
+    scores -= np.max(scores)
+    score_correct_class = scores[y[i]]
+
+    """dL_i/ds_j = 
+      if j == y_i:
+        -1 + (1/sum(e^s_j)) * e^(s_j)
+      if j != y_i:
+        (1/sum(e^s_j)) * e^(s_j)
+        
+      dL_i/dW = dL_i/ds_j * ds_j/dW
+      if j == y_i:
+        (-1 + (1/sum(e^s_j)) * e^(s_j)) * x_i
+      if j != y_i:
+        ((1/sum(e^s_j)) * e^(s_j)) * x_i"""
+    
+    for j in range(len(scores)):
+      f = np.exp(scores[j])/np.sum(np.exp(scores))
+      if j == y[i]:
+        f -= 1
+        dW[:, y[i]] += f * X[i]
+      else:
+        dW[:, j] += f * X[i]
+
+    loss += -score_correct_class + np.log(np.sum(np.exp(scores)))
+
+  """ Right now the loss is a sum over all training examples, but we want it
+    to be an average instead so we divide by num_train. This is L_i."""
+  loss /= len(X)
+  dW /= len(X)
+
+  """L = L_i + regularization"""
+  # Add regularization to the loss.
+  loss += reg * np.sum(W * W)
+  dW += 2* reg * W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -53,7 +93,14 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  scores = X.dot(W)
+  scores_y = scores[range(len(X)), y][: np.newaxis]
+  loss = np.sum(-scores_y + np.log(np.sum(np.exp(scores))))/len(X) + (reg * np.sum(W*W))
+
+  f = np.exp(scores)/np.sum(np.exp(scores))
+  f[range(len(X)), y] -= 1                    # gradient for correct class
+  dW = X.T.dot(f)/len(X) + (2 * reg * W)      # gradient for incorrect class
+
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################